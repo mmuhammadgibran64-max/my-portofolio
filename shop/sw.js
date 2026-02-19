0ae055bd7d5ba1ed6fa109aa00ccbe07399ee3c2
Muhammad Gibran <mmuhammadgibran64@gmail.com>
2026-02-19 07:54:41 +0700

    Delete shop directory

--- shop/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'gibran-v1';
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
